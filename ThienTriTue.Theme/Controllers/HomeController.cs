30804db527ceea02b412541e9dfdd2dbb060c0ae
Nguyen Duc Quan <quannd@bizsys.vn>
2025-11-09 17:29:19 +0700

    Add + Set Export Web Pages in Views

--- ThienTriTue.Theme/Controllers/HomeController.cs
+++ ThienTriTue.Theme/Controllers/HomeController.cs
@@ -7,15 +7,18 @@ namespace ThienTriTue.Theme.Controllers;
 [Feature("ThienTriTue.Theme")]
 public class HomeController : Controller
 {
+    // Dùng root ~/Views để phù hợp với view lookup của Razor/OrchardCore (embedded/compiled views)
+    private const string ThemeRoot = "~/Views";
+
     [HttpGet("/")]
-    public IActionResult Index()
-    {
-        return View("~/Views/Home/Index.cshtml");
-    }
+    public IActionResult Index() => View($"{ThemeRoot}/Home/Index.cshtml");
 
     [HttpGet("/giao-duc")]
-    public IActionResult Education()
-    {
-        return View("~/Views/Education/Index.cshtml");
-    }
+    public IActionResult Education() => View($"{ThemeRoot}/Education/Index.cshtml");
+
+    [HttpGet("/phat-giao")]
+    public IActionResult Budha() => View($"{ThemeRoot}/Buddha/Index.cshtml");
+
+    [HttpGet("/nhan-qua")]
+    public IActionResult Morals() => View($"{ThemeRoot}/Morals/Index.cshtml");
 }
