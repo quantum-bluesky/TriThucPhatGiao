1110ef8c55742b1a415d24b442a4915911a26aee
Nguyen Duc Quan <quannd@bizsys.vn>
2025-11-09 21:41:56 +0700

    Add postcss config

---
+++ ThienTriTue.Web/gulpfile.js
@@ -0,0 +1,16 @@
+﻿var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var atImport = require('postcss-import');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
+gulp.task('css', function () {
+    var plugins = [
+        atImport,
+        autoprefixer,
+        cssnano
+    ];
+    return gulp.src('./Styles/app.css')
+        .pipe(postcss(plugins))
+        .pipe(gulp.dest('./wwwroot/css'));
+});
--- ThienTriTue.Web/tailwind.config.js
+++ ThienTriTue.Web/tailwind.config.js
@@ -1,20 +1,22 @@
 // ThienTriTue.Web/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./Areas/**/*.{cshtml,js,html}",
-    "./Views/**/*.{cshtml,js,html}",
-    "./Pages/**/*.{cshtml,js,html}",
-    "../ThienTriTue.Theme/Views/**/*.{cshtml,js,html}",
-    "./wwwroot/js/**/*.js",
-    "./wwwroot/html/**/*.html" 
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        'inter': ['Inter', 'sans-serif'],
-      },
+    content: [
+        "./Areas/**/*.{cshtml,js,html}",
+        "./Views/**/*.{cshtml,js,html}",
+        "./Pages/**/*.{cshtml,js,html}",
+        "../ThienTriTue.Theme/Views/**/*.{cshtml,js,html}",
+        "./wwwroot/js/**/*.js",
+        "./wwwroot/html/**/*.html"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                'inter': ['Inter', 'sans-serif'],
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: {
+        "@tailwindcss/postcss": {},
+    },
 }
